7e9ac0e058c2a16388a146e93ca869a20e50577d
meritozh <ah841814092@gmail.com>
2022-01-13 16:07:38 +0800

    feat: add anti-aliasing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod camera;
 pub mod hit;
 pub mod hit_list;
 pub mod ray;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use glam::DVec3;
 use image::ImageBuffer;
+use rand::{distributions::Uniform, prelude::Distribution};
 use ryt::{
+    camera::Camera,
     hit_list::HittableList,
-    ray::{ray_color, Point3, Ray},
+    ray::{ray_color, Color, Point3},
     sphere::Sphere,
 };
 
@@ -11,35 +12,36 @@ fn main() {
     let image_width = 400;
     let image_height: u32 = ((image_width as f64 / aspect_ratio).floor()) as u32;
 
+    let samples_per_pixel = 100.0;
+    let scale = 1.0 / samples_per_pixel;
+
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = DVec3::new(viewport_width, 0.0, 0.0);
-    let vertical = DVec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - DVec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     let mut image = ImageBuffer::new(image_width, image_height);
 
+    let mut rng = rand::thread_rng();
+    let chaos = Uniform::new(0.0, 1.0);
+
     for (x, y, pixel) in image.enumerate_pixels_mut() {
-        let u = x as f64 / (image_width - 1) as f64;
-        let v = 1.0 - (y as f64 / (image_height - 1) as f64);
-        let r = Ray {
-            origin,
-            direction: lower_left_corner + u * horizontal + v * vertical - origin,
-        };
-        let color: [u8; 3] = ray_color(&r, &world)
-            .to_array()
-            .map(|val| (val * 255.999) as u8)
-            .try_into()
-            .unwrap();
-        *pixel = image::Rgb(color);
+        let mut color = Color::new(0.0, 0.0, 0.0);
+        (0..samples_per_pixel as i64).for_each(|_| {
+            let u = (x as f64 + chaos.sample(&mut rng)) / (image_width - 1) as f64;
+            let v = 1.0 - ((y as f64 + chaos.sample(&mut rng)) / (image_height - 1) as f64);
+            let ray = camera.get_ray(u, v);
+
+            color += ray_color(&ray, &world);
+
+            let color = color
+                .to_array()
+                .map(|val| val * scale)
+                .map(|val| (256.0 * val.clamp(0.0, 0.999)) as u8);
+
+            *pixel = image::Rgb(color);
+        });
     }
 
     image.save("output.png").unwrap();
