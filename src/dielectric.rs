6b02ebda0604044782a6a0779fabf4c310d6e623
meritozh <ah841814092@gmail.com>
2022-01-19 20:26:30 +0800

    fix: error refract result

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -9,7 +9,7 @@ use crate::{
 fn refract(uv: DVec3, n: DVec3, etai_over_etat: f64) -> DVec3 {
     let cos_theta = -uv.dot(n).min(1.0);
     let r_out_perp = etai_over_etat * (uv + cos_theta * n);
-    let r_out_parallel = (1.0 - r_out_perp.length_squared()).abs().sqrt() * n;
+    let r_out_parallel = -(1.0 - r_out_perp.length_squared()).abs().sqrt() * n;
     return r_out_perp + r_out_parallel;
 }
 
